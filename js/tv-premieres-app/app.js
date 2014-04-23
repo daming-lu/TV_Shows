ce09c385abf94cebea34bd566e067b7a80a866e0
damingl <damingl@zoosk.com>
2014-04-23 16:58:13 -0700

    JSON requery works

--- js/tv-premieres-app/app.js
+++ js/tv-premieres-app/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('TVPremieresApp',[]);
+/*
+var print = function(o){
+    var str='';
+
+    for(var p in o){
+        if(typeof o[p] == 'string'){
+            str+= p + ': ' + o[p]+'; </br>';
+        }else{
+            str+= p + ': { </br>' + print(o[p]) + '}';
+        }
+    }
+    str += '<hr>';
+    return str;
+}
+*/
--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,8 +1,22 @@
 app.controller("mainController", function($scope, $http){
 
+    $scope.apiKey = "098e28d7f07bc26e63fc5e8b0b700e13";
     $scope.init = function() {
+        //API requires a start date
+        var today = new Date();
+        //Create the date string and ensure leading zeros if required
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+            console.log(data);
+            //$('body').append( print(data) );
 
+        }).error(function(error) {
+ 
+        });
     };
 
 });
+
+
+
 
