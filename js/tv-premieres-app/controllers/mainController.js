57dfc458c395ecf2603bd27f91fd68d0f066a85b
damingl <damingl@zoosk.com>
2014-04-23 18:01:48 -0700

    empty search box added

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -2,17 +2,16 @@ app.controller("mainController", function($scope, $http){
 
     $scope.apiKey = "098e28d7f07bc26e63fc5e8b0b700e13";
     $scope.results = [];
+    $scope.filterText = null;
+    
     $scope.init = function() {
         //API requires a start date
         var today = new Date();
         //Create the date string and ensure leading zeros if required
         var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
         $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
-            //console.dir(data);
             console.log(JSON.stringify(data));
-            
-            //$('body').append( print(data) );
-            
+                    
             //As we are getting our data from an external source, we need to format the data so we can use it to our desired effect
             //For each day, get all the episodes
             angular.forEach(data, function(value, index){
